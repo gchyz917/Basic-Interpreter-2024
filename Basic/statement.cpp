2cd9aa75e8dd548c45b0ca0106f1b604ba923306
JaneZ <zhuyihan0826@outlook.com>
2024-12-02 14:30:43 +0800

    Update statement.cpp

--- Basic/statement.cpp
+++ Basic/statement.cpp
@@ -27,9 +27,7 @@ Assignment::Assignment(const std::string &variable,Expression *expr) {
     this->varname=variable;
     this->expression=expr;
 }
-Assignment::~Assignment() {
-    delete []expression;
-}
+Assignment::~Assignment()=default;
 void Assignment::execute(EvalState &state, Program &program) {
     int value=expression->eval(state);
     state.setValue(varname,value);//对新定义的变量进行赋值
@@ -39,13 +37,7 @@ void Assignment::execute(EvalState &state, Program &program) {
 printthings::printthings(const std::vector<Expression*>& exprs):expressions_(exprs) {
     this->expressions_=exprs;
 }
-printthings::~printthings() {
-    for(auto expr:expressions_) {
-        delete []expr;
-    }
-    expressions_.clear();
-    
-}
+printthings::~printthings()=default;
 void printthings::execute(EvalState &state, Program &program) {
     //std::cout << "Executing print statement" << std::endl;
     for(auto expr:expressions_) {
@@ -64,6 +56,7 @@ void printthings::execute(EvalState &state, Program &program) {
 inputthings::inputthings(const std::string variable) {
     this->varname=variable;
 }
+inputthings::~inputthings()=default;
 void inputthings::execute(EvalState &state, Program &program) {
     std::string input;
     while(true) {
@@ -84,10 +77,7 @@ IFStatement::IFStatement(Expression *conditions, Statement *thenthings) {
     this->condition=conditions;
     this->then=thenthings;
 }
-IFStatement::~IFStatement() {
-    delete condition;
-    delete then;
-}
+IFStatement::~IFStatement() =default;
 void IFStatement::execute(EvalState &state, Program &program) {
     if(condition->eval(state)!=0) {
         then->execute(state,program);
@@ -98,6 +88,7 @@ void IFStatement::execute(EvalState &state, Program &program) {
 GOTOStatement::GOTOStatement(int number) {
     this->linenumber=number;
 }
+GOTOStatement::~GOTOStatement()=default;
 void GOTOStatement::execute(EvalState &state, Program &program) {
     program.gotoline(linenumber);//跳转到linenumber那行并执行
 }
